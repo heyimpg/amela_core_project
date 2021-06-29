a62f8e44216a026c8087435b59280e8fc828add8
OOP <tomodachy2000@gmail.com>
2021-06-29 11:04:13 +0700

    max-value-matrix

---
+++ first_project/src/java_web/max_value_matrix.java
@@ -0,0 +1,18 @@
+package java_web;
+
+public class max_value_matrix {
+    public static void main(String[] args) {
+        int[][] matrix = {{1,2,3},{4,5,3}};
+        int maxValue = matrix[0][0];
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (maxValue<matrix[i][j])
+                    maxValue = matrix[i][j];
+                System.out.print(matrix[i][j]+"\t");
+            }
+            System.out.println();
+        }
+        System.out.println("Max value in matrix : "+maxValue);
+        System.out.println("\n\n\n--------End Code--------");
+    }
+}
