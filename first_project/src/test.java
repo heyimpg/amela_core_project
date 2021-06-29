2bc4a8269b6da5c89ac52c497ac177dc98d3d20d
OOP <tomodachy2000@gmail.com>
2021-06-29 10:13:30 +0700

    vice_versa_number

--- first_project/src/test.java
+++ first_project/src/test.java
@@ -1,6 +1,14 @@
 public class test {
     public static void main(String[] args) {
+        int x = 5;
 
+        do{
+
+            System.out.println("a");
+
+            x--;
+
+        } while(x == 5);
     }
 
 
