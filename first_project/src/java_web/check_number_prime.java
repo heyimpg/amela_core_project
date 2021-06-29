874d65916d2938ddcfbbf292813c82dde691abd9
OOP <tomodachy2000@gmail.com>
2021-06-29 09:42:55 +0700

    number_prime

---
+++ first_project/src/java_web/check_number_prime.java
@@ -0,0 +1,27 @@
+package java_web;
+
+import java.util.Scanner;
+
+public class check_number_prime {
+    public static void main(String[] args) {
+        int number;
+        Scanner scanner  = new Scanner(System.in);
+        System.out.println("Enter your number: "); number = scanner.nextInt();
+
+        if (number < 2)
+        {
+            System.out.println(number + " is not a prime");
+            return;
+        }
+        for (int i = 2; i <= Math.sqrt(number); i++) {
+            if (number%i==0)
+            {
+                System.out.println(number + " is not a prime");
+                return;
+            }
+        }
+        System.out.println(number + " is a prime");
+
+        System.out.println("\n\n\n--------End Code--------");
+    }
+}
