ab8086b95bae14394d7d737d9f57d91073ce4f56
OOP <tomodachy2000@gmail.com>
2021-06-30 22:02:05 +0700

    interface-by-comparable

---
+++ first_project/src/java_web/branches_ex/ComparableCircle.java
@@ -0,0 +1,19 @@
+package java_web.branches_ex;
+
+public class ComparableCircle  extends  Circle   implements Comparable<ComparableCircle>{
+
+    public ComparableCircle() {
+    }
+
+    public ComparableCircle(double radius) {
+        super(radius);
+    }
+
+    @Override
+    public int compareTo(ComparableCircle o) {
+        if (getRadius() > o.getRadius()) return 1;
+        else if (getRadius() < o.getRadius()) return -1;
+        else return 1;
+      //  return 1;
+    }
+}
---
+++ first_project/src/java_web/branches_ex/mainClass.java
@@ -0,0 +1,27 @@
+package java_web.branches_ex;
+
+import java.util.Arrays;
+
+public class mainClass {
+    public static void main(String[] args) {
+        ComparableCircle[] circles = new ComparableCircle[4];
+        circles[0] = new ComparableCircle(3.6);
+        circles[1] = new ComparableCircle();
+        circles[2] = new ComparableCircle(3.9);
+        circles[3] = new ComparableCircle(2.5);
+
+        System.out.println("Pre-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+
+        Arrays.sort(circles);
+
+        System.out.println("After-sorted:");
+        for (ComparableCircle circle : circles) {
+            System.out.println(circle);
+        }
+
+        System.out.println("\n\n\n--------End Code--------");
+    }
+}
