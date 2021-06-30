019738feccb689529311d8e70f1aae8da2c761e0
OOP <tomodachy2000@gmail.com>
2021-06-30 10:05:49 +0700

    enherit-circle-cylinder

---
+++ first_project/src/java_web/Inherit_cicle_cylinder/Cylinder.java
@@ -0,0 +1,44 @@
+package java_web.Inherit_cicle_cylinder;
+
+public class Cylinder extends Circle {
+    private double height;
+
+    public Cylinder()
+    {
+
+    }
+    public Cylinder(double height) {
+        this.height = height;
+    }
+
+    public Cylinder(double radius, String color, double height) {
+        super(radius, color);
+        this.height = height;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    @Override
+    public double getArea() {
+        return 2*Math.PI*getRadius()*height;
+    }
+    public double getVolume()
+    {
+        return getArea()*height;
+    }
+
+    @Override
+    public String toString() {
+        return "Cylinder{" +
+                "radius= "+getRadius() +
+                ", color= "+getColor() +
+                ", height=" + height +
+                '}';
+    }
+}
---
+++ first_project/src/java_web/Inherit_cicle_cylinder/mainClass.java
@@ -0,0 +1,31 @@
+package java_web.Inherit_cicle_cylinder;
+
+public class mainClass {
+    public static void main(String[] args) {
+        //Kiểm thử 1 : với hình tròn
+        Circle circle =  new Circle();
+        System.out.println(circle);
+
+        circle =  new Circle(2.5, "red");
+        System.out.println(circle);
+        System.out.printf("Circle Area = %.2f", circle.getArea() );
+        System.out.printf("\nCircle Perimeter = %.2f", circle.getPerimeter());
+
+        System.out.println("\n------------------------------------------\n");
+
+        //Kiểm thử 2: với hình trụ
+        Cylinder cylinder = new Cylinder();
+        System.out.println(cylinder);
+
+        cylinder= new Cylinder(3.5);
+        System.out.println(cylinder);
+
+        cylinder= new Cylinder(3.5, "blue", 1.5);
+        System.out.println(circle);
+        System.out.printf("Cylinder Area = %.2f", cylinder.getArea());
+        System.out.printf("\nCylinder Volume =  %.2f", cylinder.getVolume());
+
+
+        System.out.println("\n\n\n--------End Code--------");
+    }
+}
