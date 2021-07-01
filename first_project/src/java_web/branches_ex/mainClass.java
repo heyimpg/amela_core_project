7cbbfbd1126bb2bd938b8088c765201405ec1053
OOP <tomodachy2000@gmail.com>
2021-07-01 09:16:22 +0700

    refactor-variable-const

---
+++ first_project/src/java_web/branches_ex/mainClass.java
@@ -0,0 +1,10 @@
+package java_web.branches_ex;
+
+public class mainClass {
+    public static void main(String[] args) {
+        //Kiểm thử
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        System.out.println(fizzBuzz.fizzBuzz(6));
+        System.out.println("\n\n\n--------End Code--------");
+    }
+}
