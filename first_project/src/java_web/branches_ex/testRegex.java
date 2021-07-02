30393f18d14462020f73978811ffa27e968bb5e1
OOP <tomodachy2000@gmail.com>
2021-07-02 09:21:20 +0700

    regex-acc-code

---
+++ first_project/src/java_web/branches_ex/testRegex.java
@@ -0,0 +1,9 @@
+package java_web.branches_ex;
+
+public class testRegex {
+    public static void main(String[] args) {
+        boolean check =  "java^".matches("\\w+");
+        System.out.println(check);
+        System.out.println("\n\n\n--------End Code--------");
+    }
+}
