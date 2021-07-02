19d5162e28df021a43e7668a6827274efd2ae01f
OOP <tomodachy2000@gmail.com>
2021-07-02 16:50:19 +0700

    advise ignore

---
+++ first_project/src/java_web/branches_ex/mainClass.java
@@ -0,0 +1,8 @@
+package java_web.branches_ex;
+
+public class mainClass {
+    public static void main(String[] args) {
+        System.out.println("OK nha");
+        System.out.println("\n\n\n--------End Code--------");
+    }
+}
