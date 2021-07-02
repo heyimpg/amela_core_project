102debe244a3d6aa4dca11d019ccb6a29a131070
OOP <tomodachy2000@gmail.com>
2021-07-02 16:55:14 +0700

    advise gitIgnore 2

--- first_project/src/java_web/branches_ex/mainClass.java
+++
@@ -1,8 +0,0 @@
-package java_web.branches_ex;
-
-public class mainClass {
-    public static void main(String[] args) {
-        System.out.println("OK nha");
-        System.out.println("\n\n\n--------End Code--------");
-    }
-}
