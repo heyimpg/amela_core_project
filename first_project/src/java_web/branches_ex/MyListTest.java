f24ac0d460ec21b4e2cb0b637521c42c13ab9324
OOP <tomodachy2000@gmail.com>
2021-07-05 16:20:33 +0700

    list-code

---
+++ first_project/src/java_web/branches_ex/MyListTest.java
@@ -0,0 +1,23 @@
+package java_web.branches_ex;
+
+public class MyListTest {
+    public static void main(String[] args) {
+
+        //Kiểm thử
+        MyList<Integer> listInteger = new MyList<Integer>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(3);
+        listInteger.add(4);
+
+        System.out.println("element 4: "+listInteger.get(4));
+        System.out.println("element 1: "+listInteger.get(1));
+        System.out.println("element 2: "+listInteger.get(2));
+
+        /*listInteger.get(6);
+        System.out.println("element 6: "+listInteger.get(6));*/
+
+        System.out.println("\n\n\n--------End Code--------");
+    }
+}
